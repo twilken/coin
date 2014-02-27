ea5409813f87fac8ce166a974106e844d308aa43
tenpeoplemeet <tenpeoplemeet@gmail.com>
2014-02-27 16:38:55 +0100

    Add basic coin throw functionality

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-document.write("javascript");
+window.onload = init;
+
+function throwCoin() {
+	var coin = document.getElementById("coin");
+	var rand = Math.floor(Math.random()*2)
+	if (rand == 0) {
+		coin.firstChild.nodeValue = "HEADS";
+	} else {
+		coin.firstChild.nodeValue = "TAILS";
+  	}
+}
+
+function init() {
+	var button = document.getElementById("button");
+	button.addEventListener("click", throwCoin, false);
+}
