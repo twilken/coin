50ea2dac72683133c301687dc852ffd30cd2bcee
tenpeoplemeet <tenpeoplemeet@gmail.com>
2014-02-27 20:01:04 +0100

    Change button to clickable picture

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 window.onload = init;
 
+const heads = "heads.jpg"
+const tails = "tails.png"
+
 function throwCoin() {
 	var coin = document.getElementById("coin");
-	var rand = Math.floor(Math.random()*2)
-	if (rand == 0) {
-		coin.firstChild.nodeValue = "HEADS";
-	} else {
-		coin.firstChild.nodeValue = "TAILS";
-  	}
+	var rand = Math.floor(Math.random()*2);
+	var image = rand == 0 ? heads : tails;
+	document["coin"].src = image;
 }
 
 function init() {
-	var button = document.getElementById("button");
-	button.addEventListener("click", throwCoin, false);
+	var coin = document.getElementById("coin");
+	coin.addEventListener("click", throwCoin, false);
 }
